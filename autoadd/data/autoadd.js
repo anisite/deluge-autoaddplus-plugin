754ce3b17c79e67408f39022894aebea9cd5b9fd
Dany Cote <infologique@gmail.com>
2016-04-16 09:49:04 -0400

    init
    initial changes

--- autoadd/data/autoadd.js
+++ autoadd/data/autoadd.js
@@ -31,12 +31,12 @@ Copyright:
     statement from all source files in the program, then also delete it here.
 */
 
-AutoAddPlugin = Ext.extend(Deluge.Plugin, {
+AutoAddPluginPP = Ext.extend(Deluge.Plugin, {
 	constructor: function(config) {
 		config = Ext.apply({
-			name: "AutoAdd"
+			name: "AutoAdd++"
 		}, config);
-		AutoAddPlugin.superclass.constructor.call(this, config);
+		AutoAddPluginPP.superclass.constructor.call(this, config);
 	},
 	
 	onDisable: function() {
@@ -47,4 +47,4 @@ AutoAddPlugin = Ext.extend(Deluge.Plugin, {
 		
 	}
 });
-new AutoAddPlugin();
+new AutoAddPluginPP();
